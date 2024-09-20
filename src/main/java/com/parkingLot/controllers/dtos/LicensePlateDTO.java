6b9cd4917a361b0d3778659448915c4f71c7267d
Sandra Mendes <sandra.mendes@gmail.com>
2024-09-20 10:07:54 -0300

    add parking application

---
+++ src/main/java/com/parkingLot/controllers/dtos/LicensePlateDTO.java
@@ -0,0 +1,15 @@
+package com.parkingLot.controllers.dtos;
+
+public class LicensePlateDTO {
+
+    private String placa;
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+
+}
